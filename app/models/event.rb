89b8f25b0452ccb818f97d822b92ef4d77aa4eec
christophetrannoy <christophe.trannoy@gmail.com>
2019-05-17 12:59:48 +0200

    ca va

---
+++ app/models/event.rb
@@ -0,0 +1,18 @@
+class Event < ApplicationRecord
+  validates :name, presence: true
+  validate :d_erreur
+  validate :d_today
+
+  def d_erreur
+    if endDate < startDate
+      errors.add(:endDate, '/ La date de fin doit être choisie après la date de début.')
+    end
+  end
+
+  def d_today
+    if startDate < Time.new
+      errors.add(:startDate, '/ La date de début doit être au moins choisie après la date de d\'aujourd\'hui.')
+    end
+  end
+
+end
